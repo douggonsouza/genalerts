9045e872e65f03fc1ddfd2a210f32a40eb1768f5
Douglas G. de Souza <douggonsouza@gmail.com>
2023-04-07 15:28:16 -0300

    Inicio do repositório.

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'douggonsouza/genalerts',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '34f5174ce6da6c6c2467f0129774e63c2bd5fe7c',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'douggonsouza/genalerts' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '34f5174ce6da6c6c2467f0129774e63c2bd5fe7c',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
